5ec34d1a8fb7cf9c7d53293179de7b4621f50a13
Krishnavamsifsd <Krishnavamsifsd@gmail.com>
2023-01-31 13:39:18 +0530

    updated

--- src/Components/CourseCard/CourseCard.js
+++ src/Components/CourseCard/CourseCard.js
@@ -1,49 +1,47 @@
 import React from "react";
-import { Card, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 const CoursesCard = ({ course }) => {
-    const { name, background, price, instructor, _id, email, enrollUser ,company } = course;
-    return (
-        <article className="col col-lg-4" lg="4" data-aos="zoom-out-up">
-            <Card className="my-3 mx-auto shadow">
-                <Card.Img variant="top" src={background} />
-                <Card.Body>
-                    <Card.Title className="fw-bold">{name}</Card.Title>
-                    <Card.Title>Price: ${price}</Card.Title>
-                    <div className='fw-light'>
-                    <small>Company: {company}</small> <br />
-                        <small>Instructor: {instructor}</small> <br />
-                        <small>Contact: {email}</small>
-                    </div>
-                    <div className='d-flex justify-content-between mt-2'>
-                        <div>
-                            <i className="fas fa-star text-warning"></i>
-                            <i className="fas fa-star text-warning"></i>
-                            <i className="fas fa-star text-warning"></i>
-                            <i className="fas fa-star text-warning"></i>
-                            <i className="far fa-star text-warning"></i>
-                        </div>
-                        <div>
-                            <i className="fas fa-user me-1"></i>
-                            <small>{enrollUser}</small>
-                        </div>
-                    </div>
-                    <OverlayTrigger
-                        overlay={
-                            <Tooltip id={_id}>
-                                Take Course <strong>{name}</strong>.
-                            </Tooltip>
-                        }
-                    >
-                        <button className="mt-3 btn btn-dark w-100 text-capitalize fs-6 mt-1">
-                            <i className="fas fa-cart-plus me-2"></i>
-                            Enroll Now
-                        </button>
-                    </OverlayTrigger>
-                </Card.Body>
-            </Card>
-        </article>
-    );
+  const { name, background, price, instructor, email, enrollUser, company } = course;
+  const sendMessage = () => {
+    window.location.href = `https://wa.me/+918309702035?text=I%20am%20interested%20in%20enrolling%20in%20${name}%20course%20offered%20by%20${company}`;
+  };
+  return (
+    <article className="col col-lg-4" lg="4" data-aos="zoom-out-up">
+      <Card className="my-3 mx-auto shadow">
+        <Card.Img variant="top" src={background} />
+        <Card.Body>
+          <Card.Title className="fw-bold">{name}</Card.Title>
+          <Card.Title>Price: ${price}</Card.Title>
+          <div className="fw-light">
+            <small>Company: {company}</small> <br />
+            <small>Instructor: {instructor}</small> <br />
+            <small>Contact: {email}</small>
+          </div>
+          <div className="d-flex justify-content-between mt-2">
+            <div>
+              <i className="fas fa-star text-warning"></i>
+              <i className="fas fa-star text-warning"></i>
+              <i className="fas fa-star text-warning"></i>
+              <i className="fas fa-star text-warning"></i>
+              <i className="far fa-star text-warning"></i>
+            </div>
+            <div>
+              <i className="fas fa-user me-1"></i>
+              <small>{enrollUser}</small>
+            </div>
+          </div>
+          <button
+            className="mt-3 btn btn-dark w-100 text-capitalize fs-6 mt-1"
+            onClick={sendMessage}
+          >
+            <i className="fas fa-cart-plus me-2"></i>
+            Enroll Now
+          </button>
+        </Card.Body>
+      </Card>
+    </article>
+  );
 };
 
 export default CoursesCard;
