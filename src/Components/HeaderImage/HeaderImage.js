90688b4dfd793868367738fda8ec21b0cf014869
krishna vamsi <123242698+Krishnavamsifsd@users.noreply.github.com>
2023-02-02 13:50:20 +0530

    Update HeaderImage.js

--- src/Components/HeaderImage/HeaderImage.js
+++ src/Components/HeaderImage/HeaderImage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-
-const HeaderImage = ({title, button}) => {
+import "./Headerimg.css"
+const HeaderImage = ({ title, button }) => {
     return (
         <div>
             <Row
                 data-aos="zoom-out-up"
-                className="d-flex align-items-center bgImg"
+                className="header-row d-flex align-items-center bgImg"
                 style={{ height: "600px" }}
             >
                 <Col md={6} className="ms-5">
